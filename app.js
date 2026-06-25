2186453715c053dc92bb70be3208d53fd4fc5e62
kevin-guerrero <kevin.guerrero.7e6@itb.cat>
2026-06-25 21:06:39 +0200

    feat: dividir el codigo en funciones - cambiar el evento  a async

--- app.js
+++ app.js
@@ -4,36 +4,50 @@ const weatherSection = document.getElementById("weatherSection");
 // Boton submit
 const inputEnviar = document.getElementById("submit");
 
-// Función asincrona para obtener el tiempo de una ciudad buscada
+/**
+ * Función asincrona que hace la llamada a la API para obtener el tiempo de una ciudad buscada por el usuario.
+ * @author Kevin Guerrero
+ * @param {String} ciudadBuscada 
+ * @returns JSON con los datos del tiempo
+ */
 async function obtenerTiempo(ciudadBuscada) {
     try {
         // Llamar a la API
         const respuesta = await fetch(`/api/weather?city=${ciudadBuscada}`);
         const datosDelTiempo = await respuesta.json();
 
-        // Resulados de la respuesta que devuelve OpenWeatherMap en la consola
-        //console.log(datosDelTiempo);
-
-        // TODO: Pintar aqui los resultados o hacer una función que lo haga
-        datosDelTiempo.weather.forEach(clima => {
-            const descripcion = document.createElement('p');
-            const imagen = document.createElement('img');
-
-            descripcion.textContent = `${clima.description}`;
-            imagen.src = `https://openweathermap.org/img/wn/${clima.icon}.png`;
-            imagen.alt = `https://openweathermap.org/img/wn/${clima.description}`
-
-            weatherSection.appendChild(descripcion);
-            weatherSection.appendChild(imagen);
-        });
+        return datosDelTiempo;
 
     } catch (error) {
         console.error("Error al obtener el tiempo:", error);
     }
 };
 
+/**
+ * Función que muestra el clima en la página utilizando los datos de un JSON que recibe por parametro.
+ * @author Kevin Guerrero
+ * @param {JSON} datosDelTiempo
+ */
+
+function pintarResultados(datosDelTiempo) {
+
+    // Pintar los resultados en la página
+    datosDelTiempo.weather.forEach(clima => {
+        const descripcion = document.createElement('p');
+        const imagen = document.createElement('img');
+
+        descripcion.textContent = `${clima.description}`;
+        imagen.src = `https://openweathermap.org/img/wn/${clima.icon}.png`;
+        imagen.alt = `https://openweathermap.org/img/wn/${clima.description}`
+
+        weatherSection.appendChild(descripcion);
+        weatherSection.appendChild(imagen);
+    });
+}
+
+
 // Evento cuando el usuario introduce una ciudad
-inputEnviar.addEventListener("click", function() {
+inputEnviar.addEventListener("click", async (event)=> {
     let inputUsuari = document.getElementById("busquedad").value;
     weatherSection.style.display = "block";
     if(inputUsuari == "") {
@@ -45,9 +59,7 @@ inputEnviar.addEventListener("click", function() {
         `;
         return;
     }else {
-        obtenerTiempo(inputUsuari);
+        let datosJSON = await obtenerTiempo(inputUsuari);
+        pintarResultados(datosJSON);
     }
 });
-
-// Pruebas
-//obtenerTiempo("Barcelona")
